10e4dafc4bfd0d15913b2139582c7e5ddf8615f5
pijoh3 <42169922+pijoh3@users.noreply.github.com>
2018-08-22 16:15:16 +0900

    ch4 오류코드 수정
    ch4 예제4-4 노드 삭제부분 첫번째 노드나 마지막 노드 삭제시 삭제가 되지않는 오류가 발생하여 첫번째노드랑 마지막 노드일때를 경우를 더 나누어서 삭제함수 수정

--- ch4.c
+++ ch4.c
@@ -1,4 +1,4 @@
-//예제 4-4
+//예제 4-1
 /*#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -307,7 +307,7 @@ int main()
 }*/
 
 //예제 4-3
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -442,4 +442,121 @@ int main()
 	printList(CL);
 
 	return 0;
+}*/
+
+//예제 4-4
+/*#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct ListNode
+{
+	struct ListNode * llink;
+	char data[4];
+	struct ListNode *rlink;
+}listNode;
+
+typedef struct
+{
+	listNode * head;
+}linkedList_h;
+
+linkedList_h * createLinkedList_h()
+{
+	linkedList_h * DL;
+	DL = (linkedList_h *)malloc(sizeof(linkedList_h));
+	DL->head = NULL;
+	return DL;
 }
+
+void printList(linkedList_h * DL)
+{
+	listNode * p;
+	printf(" DL = (");
+	p = DL->head;
+	while (p != NULL)
+	{
+		printf("%s", p->data);
+		p = p->rlink;
+		if (p != NULL)printf(", ");
+	}
+	printf(")\n");
+}
+
+void insertNode(linkedList_h * DL, listNode * pre, char *x)
+{
+	listNode * newNode;
+	newNode = (listNode *)malloc(sizeof(listNode));
+	strcpy(newNode->data, x);
+	if (DL->head == NULL)
+	{
+		newNode->llink = NULL;
+		newNode->rlink = NULL;
+		DL->head = newNode;
+	}
+	else
+	{
+		newNode->rlink = pre->rlink;
+		pre->rlink = newNode;
+		newNode->llink = pre;
+		if (newNode->rlink != NULL)
+			newNode->rlink->llink = newNode;
+	}
+}
+
+void deleteNode(linkedList_h* DL, listNode* old) {
+if (DL->head == NULL) return;
+else
+{
+if (old == DL->head)
+DL->head = old->rlink;
+else if (old->rlink == NULL)
+old->llink->rlink = NULL;
+else if (old == NULL) return;
+else
+{
+old->llink->rlink = old->rlink;
+old->rlink->llink = old->llink;
+free(old);
+}
+}
+}//기존의 코드는 첫번째 노드나 마지막 노드를 삭제하면 오류 발생.첫번째 노드일때랑 마지막 노드일때인 경우를 생각
+
+listNode * searchNode(linkedList_h * DL, char * x)
+{
+	listNode * temp;
+	temp = DL->head;
+	while (temp != NULL)
+	{
+		if (strcmp(temp->data, x) == 0)return temp;
+		else temp = temp->rlink;
+	}
+	return temp;
+}
+
+int main()
+{
+	linkedList_h * DL;
+	listNode *p;
+	DL = createLinkedList_h();
+	printf("(1) 이중 연결 리스트 생성하기! \n");
+	printList(DL); getchar();
+
+	printf("(2) 이중 연결 리스트에 [월] 노드 삽입하기\n");
+	insertNode(DL, NULL, "월");
+	printList(DL); getchar();
+
+	printf("(3) 이중 연결 리스트의 [월] 노드 뒤에 [수] 노드 삽입하기!\n");
+	p = searchNode(DL, "월"); insertNode(DL, p, "수");
+	printList(DL); getchar();
+
+	printf("(4) 이중 연결 리스트의 [수] 노드 뒤에 [금] 노드 삽입하기!\n");
+	p = searchNode(DL, "수"); insertNode(DL, p, "금");
+	printList(DL); getchar();
+
+	printf("(5) 이중 연결 리스트에서 [수] 노드 삭제하기\n");
+	p = searchNode(DL, "수"); deleteNode(DL, p);
+	printList(DL); getchar();
+
+	return 0;
+}*/
